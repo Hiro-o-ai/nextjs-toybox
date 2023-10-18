8c9f49c964c4117f4163965e512d07f862ea2be0
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2023-10-18 17:59:46 +0900

    add comment at jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,15 @@
 export default {
+    // 何回テストに失敗するとテスト自体を中止させるかの設定 デフォルトは0
+    // bail: 1,
+    // 各テスト前にモックコール、インスタンス、コンテキスト、結果をクリアするかの設定 デファルトはfalse
     clearMocks: true,
     collectCoverage: false,
     coverageDirectory: "coverage",
     moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+    // テスト環境の設定 デフォルトはNode.js フロントエンドでブラウザ環境にしたい場合はjsdom
     testEnvironment: "jest-environment-jsdom",
     transform: { "^.+\\.(ts|tsx)$": ["esbuild-jest", { sourcemap: true }] },
+    // 全てのテストで共通のコードを記述したファイルを指定
     setupFilesAfterEnv: ["./jest.setup.ts"],
     reporters: [
       "default",
